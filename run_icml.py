e3519923ca9aae2f60fccc8b5882c5ea6b73a400
Nicklas Koch Rasmussen <nicra@itu.dk>
2024-04-08 21:23:41 +0200

    working on baselines

--- run_icml.py
+++ run_icml.py
@@ -16,7 +16,7 @@ def main():
     run.main(gym_env)
 
     ## run learning experiment
-    run_learning_experiment.main(gym_env, abstraction=True)
+    # run_learning_experiment.main(gym_env, abstraction=True)
 
 if __name__ == "__main__":
     main()
