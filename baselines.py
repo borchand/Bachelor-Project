d08edad8bf6059a1de4b8555088b7a4c03ae65c0
Nicklas Koch Rasmussen <nicra@itu.dk>
2024-04-13 13:22:25 +0200

    training agents

--- baselines.py
+++ baselines.py
@@ -34,9 +34,19 @@ def _get_model_class(algo_name):
     else:
         raise ValueError('Invalid algorithm name')
 
-def main(env_name, algo_name, render_mode, timesteps):
-    
-    env = gym.make(env_name, render_mode = "rgb_array")
+def main(env_name, algo_name, timesteps: int, render = False, save = True, train = True):
+    """
+    Args:
+        :param env_name (str): Name of the environment
+        :param algo_name (str): Name of the algorithm
+            Choices: { 'ppo', 'a2c', 'dqn', 'sac', 'td3', 'ddpg' }
+        :param timesteps (int): Number of timesteps to train the model
+        :param render = False (bool): If True, render the environment
+        :param save = True (bool): If True, save the trained model
+        :param train = True (bool): If True, train the model
+    """
+    save_name = "rl-trained-agents/"+ algo_name + "_" + env_name
+    env = gym.make(env_name)
 
     # get the model class from the algo name
     model_class = _get_model_class(algo_name)
@@ -45,36 +55,63 @@ def main(env_name, algo_name, render_mode, timesteps):
     model = model_class("MlpPolicy", env=env, verbose=1)
 
     # model learn the env
-    model.learn(total_timesteps=timesteps, log_interval=4)
-    
+    if train:
+        model.learn(total_timesteps=timesteps, log_interval=10)
     # save the model
-    save_name = "rl-trained-agents/"+ algo_name + "_" + env_name
-    model.save(save_name)
+    
+    if save:
+        model.save(save_name)
 
-    # delete trained model to demonstrate loading
-    # del model 
 
-    # load the model
-    # model = model_class.load(save_name)
+    if render:
+        eval_env = gym.make(env_name, render_mode='human')
+        
+        model = model.load(save_name, env=eval_env)
 
-    # obs, info = env.reset()
-    # while True:
-    #     action, _states = model.predict(obs, deterministic=True)
-    #     obs, reward, terminated, truncated, info = env.step(action)
-    #     if terminated or truncated:
-    #         obs, info = env.reset()
-    #         break
-    #     env.render()
+        model.set_env(eval_env)
+        obs, info = eval_env.reset()
+        
+        for _ in range(1000):
+            action, _states = model.predict(obs, deterministic=True)
+            obs, reward, terminated, truncated, info = eval_env.step(action)
+            if terminated or truncated:
+                obs, info = eval_env.reset()
+                break
+            eval_env.render()
 
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(description='Set options for training and rendering CAT_RL')
     
     parser.add_argument('-e', '--env', default='CartPole-v1', help='Environment to train on')
-    parser.add_argument('-a', '--algo', default='a2c', help='Algorithm to use')
-    parser.add_argument('-t', '--time-steps', default=2000, help='Number of time steps to train the model for', type=int)
-    parser.add_argument('-r', '--render', choices=['t', 'f'], default='t', help='Render the model')
+    parser.add_argument('-a', '--algo', default='dqn', help='Algorithm to use when training')
+    # parser.add_argument('-ep', '--episodes', default=100, help='Number of episodes to train the model for', type=int)
+    parser.add_argument('-t', '--time-steps', default=None, help='Number of time steps to train the model for', type=int)
+    parser.add_argument('-tr', '--train', choices=['t', 'f'], default='t', help='Train the model')
     
+    parser.add_argument('-r', '--render', choices=['t', 'f'], default='f', help='Render the model')
+    parser.add_argument('-s', '--save', choices=['t', 'f'], default='t', help='Save the model')
+    
+    parser.add_argument('-sh', '--show', choices=['t', 'f'], default='f', help='Show the model')
+
     args = parser.parse_args()
     
-    main(args.env, args.algo, args.render == 't', args.time_steps)
+    # Render the model
+    if args.show == 't':
+        main(
+            env_name=args.env,
+            algo_name=args.algo,
+            timesteps=0,
+            render=True,
+            save=False,
+            train=False)
+    
+    # Else just train the model
+    main(
+        env_name=args.env,
+        algo_name=args.algo,
+        timesteps=args.time_steps,
+        render=args.render == 't',
+        save=args.save == 't',
+        train=args.train == 't'
+        )
