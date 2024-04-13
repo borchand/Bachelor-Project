fbf98e5dd3476e4d936b53506c8dc9ab7972d63b
Nicklas Koch Rasmussen <nicra@itu.dk>
2024-04-13 10:29:54 +0200

    training environments

--- baselines.py
+++ baselines.py
@@ -38,20 +38,17 @@ def main(env_name, algo_name, render_mode, timesteps):
     
     env = gym.make(env_name, render_mode = "rgb_array")
 
-    save_name = "rl-trianed-agents/"+ algo_name + "_MountainCar-v0"
-
     # get the model class from the algo name
     model_class = _get_model_class(algo_name)
 
     # create the model
     model = model_class("MlpPolicy", env=env, verbose=1)
 
-    # learn the model
-    env_steps = 200
-    episodes = 1000
-    total_timesteps = env_steps * episodes
-    model.learn(total_timesteps=total_timesteps, log_interval=4)
+    # model learn the env
+    model.learn(total_timesteps=timesteps, log_interval=4)
+    
     # save the model
+    save_name = "rl-trained-agents/"+ algo_name + "_MountainCar-v0"
     model.save(save_name)
 
     # delete trained model to demonstrate loading
@@ -72,10 +69,11 @@ def main(env_name, algo_name, render_mode, timesteps):
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(description='Set options for training and rendering CAT_RL')
-    parser.add_argument('-r', '--render', choices=['t', 'f'], default='t', help='Render the model')
+    
     parser.add_argument('-e', '--env', default='CartPole-v1', help='Environment to train on')
     parser.add_argument('-a', '--algo', default='a2c', help='Algorithm to use')
-    parser.add_argument('-t', '--time-steps', default=200, help='Number of time steps to train the model for')
+    parser.add_argument('-t', '--time-steps', default=2000, help='Number of time steps to train the model for')
+    parser.add_argument('-r', '--render', choices=['t', 'f'], default='t', help='Render the model')
     
     args = parser.parse_args()
     
